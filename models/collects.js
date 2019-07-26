3a41f84ce79dbae68c43b63aa315562bf2f58af7
seeyuyu <seeyuyu@163.com>
2019-07-26 23:28:54 +0800

    mongo建表有内嵌和外联之分，根据业务关系来决定，最好是内嵌，可以承担较为复杂的json格式

--- models/collects.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const {Schema,model} = mongoose;
-
-const collectSchema = new Schema({
-  __v: { type: Number, select: false },
-
-
-})
-module.exports = model("Collect",collectSchema);
--- models/users.js
+++ models/users.js
@@ -4,13 +4,32 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
   __v: { type: Number, select: false },
   name: { type: Number, required: true },
-  password: { type: String, required: true ,select:false},
-  gender: { type: String, required: true, enum: ["male", "female"], default: "male"},
-  avatar_url: { type:String},
-  locations: { type:[{type:Schema.Types.ObjectId,ref : "Location"}],select:false},
-  order: { type:[{type:Schema.Types.ObjectId,ref : "Location"}],select:false},
-  shopping_car: { type:[{type:Schema.Types.ObjectId,ref : "ShoppingCar"}],select:false},
-  collect: { type:[{type:Schema.Types.ObjectId,ref : "Collect"}],select:false},
-
+  password: { type: String, required: true, select: false },
+  gender: { type: String, required: true, enum: ["male", "female"], default: "male" },
+  avatar_url: { type: String },
+  locations: {
+    type: [{ type: Schema.Types.ObjectId, ref: "Location" }], select: false
+  },
+  order: { type: [{ type: Schema.Types.ObjectId, ref: "Order" }], select: false },
+  shopping_car: {
+    type: [
+      {
+        commodity_id: { type: String },
+        count: { type: Number, default: 0 },
+        update_time: { type: String },
+        //0为什么都没有，1为满38，之类的
+        discount_type: { type: Number, enum: [0, 1, 2, 3, 4, 5], default: 0 },
+        store_name: { type: String }
+      }
+    ], select: false
+  },
+  collect: {
+    type: [
+      {
+        commodity_id: { type: String },
+        store_name: { type: String }
+      }
+    ], select: false
+  },
 });
-module.exports = model("User",userSchema)
+module.exports = model("User", userSchema)
